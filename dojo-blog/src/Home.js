acefa10497fbb261dec70c605df52e026ed26469
Ábris rádai <abrisradai@gmail.com>
2024-03-23 11:54:24 +0100

    Finished #9

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,19 +1,19 @@
-const Home = () => {
-    const handleClick = (e) => {
-        console.log('hello, mom', e);
-    }
+import { useState } from 'react';
 
-    const handleClickAgain = (name, e) =>{
-        console.log(`hello ${name}`, e.target);
+const Home = () => {
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
+
+    const handleClick = (e) => {
+        setName('luigi');
+        setAge(30);
     }
 
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <p>{ name } is { age } years old.</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={ 
-                (e) => handleClickAgain('mario', e)
-                }>Click me again</button>
         </div>
      );
 }
